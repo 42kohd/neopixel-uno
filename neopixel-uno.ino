62969cc8b19065ad1a9be8cab9fc10b7079b1a79
Adrien Thebo <git@somethingsinistral.net>
2014-11-17 22:46:55 -0800

    Implement GlowWorm, Heating patterns

--- neopixel-uno.ino
+++ neopixel-uno.ino
@@ -39,8 +39,7 @@ struct Random : Pattern {
   virtual void apply() {
     int maxval = 16;
     for(int idx = 0; idx <= NUMPIXELS; idx++) {
-      //setPixel(idx, random(maxval), random(maxval), random(maxval));
-      m_pixels->setPixelColor(idx, random(maxval), random(maxval), random(maxval));
+      setPixel(idx, random(maxval), random(maxval), random(maxval));
     }
     m_pixels->show();
     delay(50);
@@ -51,16 +50,17 @@ struct SerialStrobe : Pattern {
   SerialStrobe(Adafruit_NeoPixel *pixels) : Pattern(pixels) { }
 
   virtual void apply() {
+    int sleep_for = 15;
     for(int idx = 0; idx < NUMPIXELS; idx++) {
       setPixel(idx, LEVEL, 0, 0);
       show();
-      delay(50);
+      delay(sleep_for);
       setPixel(idx, 0, LEVEL, 0);
       show();
-      delay(50);
+      delay(sleep_for);
       setPixel(idx, 0, 0, LEVEL);
       show();
-      delay(50);
+      delay(sleep_for);
       setPixel(idx, 0, 0, 0);
       show();
     }
@@ -82,6 +82,146 @@ struct WalkWhite : Pattern {
   }
 };
 
+struct GlowWorm : Pattern {
+  GlowWorm(Adafruit_NeoPixel *pixels) : Pattern(pixels) {
+    m_r = 0;
+    m_g = 0;
+    m_b = 0;
+  }
+
+  virtual void apply() {
+    int idx = random(NUMPIXELS);
+    int maxR = random(255);
+    int maxG = random(255);
+    int maxB = random(255);
+
+    while(up(idx, maxR, maxG, maxB));
+    while(down(idx));
+  }
+
+private:
+
+  int m_r;
+  int m_g;
+  int m_b;
+
+  void namesAreHard(int idx, float sleep) {
+    setPixel(idx, m_r, m_g, m_b);
+    show();
+    delay(sleep);
+  }
+
+  bool up(int idx, int maxR, int maxG, int maxB) {
+    bool more = false;
+
+    if(m_r < maxR) {
+      m_r++;
+      more = true;
+    }
+    if(m_g < maxG) {
+      m_g++;
+      more = true;
+    }
+    if(m_b < maxB) {
+      m_b++;
+      more = true;
+    }
+
+    namesAreHard(idx, 0.25);
+    return more;
+  }
+
+  bool down(int idx) {
+    bool more = false;
+
+    if(m_r > 0) {
+      m_r--;
+      more = true;
+    }
+    if(m_g > 0) {
+      m_g--;
+      more = true;
+    }
+    if(m_b > 0) {
+      m_b--;
+      more = true;
+    }
+
+    namesAreHard(idx, 3);
+    return more;
+  }
+
+};
+
+struct Heating : Pattern {
+  Heating(Adafruit_NeoPixel *pixels) : Pattern(pixels) {
+    m_focus = random(NUMPIXELS);
+    memset(m_elements, 0, NUMPIXELS * sizeof(int));
+
+    m_count = 100;
+  }
+
+  virtual void apply() {
+    cool();
+    heat();
+
+    for(int idx = 0; idx <= NUMPIXELS; idx++) {
+      setPixel(idx, m_elements[idx], 0, 0);
+    }
+    show();
+    delay(25);
+
+    m_count--;
+    if(m_count <= 50 && m_count > 0) {
+      m_focus = -1;
+    } else if(m_count <= 0) {
+      m_focus = random(NUMPIXELS);
+      m_count = 100;
+    }
+  }
+
+private:
+  int m_focus;
+  int m_elements[NUMPIXELS + 1];
+  int m_count;
+
+  void cool() {
+    int factor = 5;
+    for(int idx = 0; idx <= NUMPIXELS; idx++) {
+      if(m_elements[idx] > factor) {
+        m_elements[idx] -= factor;
+      } else {
+        m_elements[idx] = 0;
+      }
+    }
+  }
+
+  void heat() {
+    float scale = 0.1;
+
+    for(int idx = 0; idx <= NUMPIXELS; idx++) {
+      int heating = 0;
+      if(idx == m_focus) {
+        heating = 20;
+      } else if(idx == 0) {
+        heating = (m_elements[1] - m_elements[idx]) * 0.1;
+      } else if(idx == NUMPIXELS) {
+        heating = (m_elements[NUMPIXELS - 1] - m_elements[idx]) * 0.5;
+      } else {
+        heating += (m_elements[idx - 1] - m_elements[idx]) * 0.5;
+        heating += (m_elements[idx + 1] - m_elements[idx]) * 0.5;
+      }
+
+      m_elements[idx] += heating;
+
+      if(m_elements[idx] > 255) {
+        m_elements[idx] = 255;
+      }
+    }
+  }
+};
+
+
 void blink(int sleep) {
   digitalWrite(13, HIGH);
   delay(sleep);
@@ -89,15 +229,34 @@ void blink(int sleep) {
   delay(sleep);
 }
 
-void setup_pin13() {
-}
-
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 Pattern *pattern;
 
 void setup() {
   pinMode(13, OUTPUT);
+  //pattern = new SerialStrobe(&pixels);
+  //pattern = new GlowWorm(&pixels);
   pattern = new Random(&pixels);
+
+  randomSeed(analogRead(0));
+
+  switch(random(4)) {
+    case 3:
+      pattern = new SerialStrobe(&pixels);
+      break;
+    case 2:
+      pattern = new Random(&pixels);
+      break;
+    case 1:
+      pattern = new GlowWorm(&pixels);
+      break;
+    case 0:
+      pattern = new WalkWhite(&pixels);
+      break;
+
+  }
+  pattern = new Heating(&pixels);
+  //pattern = new WalkWhite(&pixels);
 }
 
 void loop() {
